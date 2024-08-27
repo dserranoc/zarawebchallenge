2d8c3d18a1db99ce0e82f426a98c4daf8413ecac
Daniel Serrano Cobos <d.serranoc@outlook.com>
2024-08-27 09:59:34 +0000

    feat: Create initial topbar

---
+++ src/app/components/Topbar/topbar.tsx
@@ -0,0 +1,14 @@
+import styles from './topbar.module.css'
+export default function Topbar () {
+  return (
+    <nav className={styles.nav}>
+      <div>
+        LOGO
+      </div>
+
+      <div>
+        CORAZÓN
+      </div>
+    </nav>
+  )
+}
